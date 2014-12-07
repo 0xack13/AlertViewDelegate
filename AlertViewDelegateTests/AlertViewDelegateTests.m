5db5bbcee86cf6484f88b23d656ee7a1d226b89c
Saleh Albatati <0xack13@gmail.com>
2014-12-07 20:03:08 +0300

    Initial Commit

---
+++ AlertViewDelegateTests/AlertViewDelegateTests.m
@@ -0,0 +1,34 @@
+//
+//  AlertViewDelegateTests.m
+//  AlertViewDelegateTests
+//
+//  Created by 0xack13 on 12/7/14.
+//  Copyright (c) 2014 0xack13. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AlertViewDelegateTests : XCTestCase
+
+@end
+
+@implementation AlertViewDelegateTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
